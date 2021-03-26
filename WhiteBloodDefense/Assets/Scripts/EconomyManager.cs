8c9bcfdc57243f7d644b35e18a2930411fe6edfd
jlinder742 <54907511+jlinder742@users.noreply.github.com>
2021-03-26 16:17:57 -0400

    Added UI to Game Scene

--- WhiteBloodDefense/Assets/Scripts/EconomyManager.cs
+++ WhiteBloodDefense/Assets/Scripts/EconomyManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EconomyManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<int> costs;
     public int money;
-
+    private float timer;
+    public Slider moneyBar;
+    public Font font;
+    private GUIStyle guiStyle = new GUIStyle();
     public EntityManager emScript;
 
     void Start()
     {
         emScript = gameObject.GetComponent<EntityManager>();
+        
     }
 
     // Update is called once per frame
@@ -22,6 +27,8 @@ public class EconomyManager : MonoBehaviour
         {
             BuyCell(0);
         }
+        moneyBar.value = money;
+        timer += Time.deltaTime;
     }
 
 
@@ -32,5 +39,16 @@ public class EconomyManager : MonoBehaviour
             money -= costs[cellType];
             emScript.NewPlayer(cellType);
         }
+    }
+
+    //Stuff for the timer
+    void OnGUI()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60f);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        guiStyle.fontSize = 60;
+        guiStyle.font = font;
+        GUI.Label(new Rect(555, 10, 100, 10), niceTime.ToString(),guiStyle);
     }
 }
