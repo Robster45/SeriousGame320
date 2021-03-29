7716afb77d7c42c3c2c12d2f4088ea068a2732ab
jlinder742 <54907511+jlinder742@users.noreply.github.com>
2021-03-28 21:31:04 -0400

    Added UI back

---
+++ WhiteBloodDefense/Assets/Scripts/Money_Bar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money_Bar : MonoBehaviour
+{
+    public Slider slider;
+    //getting the money from the economy script
+    public GameObject money;
+    private EconomyManager economyManager;
+    void Start()
+    {
+        economyManager = money.GetComponent<EconomyManager>();
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        //setting it
+        slider.value = economyManager.money;
+    }
+}
---
+++ WhiteBloodDefense/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float timer;
+    public Font font;
+    GUIStyle style = new GUIStyle();
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void OnGUI()
+    {
+        style.fontSize = 60;
+        style.font = font;
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        GUI.Label(new Rect(580, 10, 250, 100), niceTime, style);
+    }
+}
